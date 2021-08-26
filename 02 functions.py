f7068a33e0d278804fdacd5d719a511928ade17b
medrekm <medrek.marek@gmail.com>
2021-08-26 16:01:00 +0200

    Aktualizacja nr 1

--- 02 functions.py
+++ 02 functions.py
@@ -1,1 +1,15 @@
-#funkcion testing
+#function testing
+def noValue(param):
+    return param.__len__()
+
+def maxValue(param):
+    max=param[0]
+    for i in range(len(param)):
+        if max<param[i]:
+            max=param[i]
+    return(max)
+
+x=[1,2,3,2,1,22]
+
+print("liczba elementów w tablicy: ",noValue(x))
+print("maksymalny element w tablicy: ",maxValue(x))
